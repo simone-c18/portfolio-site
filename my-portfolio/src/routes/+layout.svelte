81ca9f231cb56c32cccc0ea137dc1e83f044a905
Simone Chrastek <simonechrastek@Simones-MacBook-Air.local>
2026-02-22 13:17:37 -0500

    implemented basic landing page and chat box

--- my-portfolio/src/routes/+layout.svelte
+++ my-portfolio/src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/cat.png';
+	import "../app.css";
 
 	let { children } = $props();
 </script>
+
+<div class="min-h-screen bg-slate-900 text-white">
+  {@render children()}
+</div>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<!-- {@render children()} -->
--- my-portfolio/src/routes/+page.svelte
+++ my-portfolio/src/routes/+page.svelte
@@ -1,13 +1,49 @@
 <script>
+    let hasEntered = true;
+
+    function enterChat(){
+        hasEntered = true;
+    }
+
 </script>
 
-<div class = "container">
-    <h1>simone chrastek</h1>
-    <h2>has invited you to chat!</h2>
-</div>
+{#if !hasEntered}
+    <div class="h-screen w-full bg-black flex flex-col items-center justify-center ">
+        <h1 class="text-white text-4xl mb-8 animate-pulse">Simone Chrastek</h1>
+        <h2>has invited you to chat!</h2>
+        
+        <div class="flex flex-row gap-4">
+            <button 
+            on:click={enterChat}
+            class="px-10 py-4 bg-mypink text-black font-bold rounded-lg hover:bg-blue-500 transition-all shadow-[0_0_20px_rgba(37,99,235,0.5)]"
+            >
+            ACCEPT
+            </button>
 
-<style>
-</style>
+            <button 
+            on:click={enterChat}
+            class="px-10 py-4 bg-mypink text-black font-bold rounded-lg hover:bg-blue-500 transition-all shadow-[0_0_20px_rgba(37,99,235,0.5)]"
+            >
+            DECLINE
+            </button>
+        </div>
+  </div>
+
+{:else}
+    <div class="h-screen w-full bg-black flex flex-col items-center justify-center p-4">
+        <div class="w-full max-w-4xl aspect-square md:aspect-video bg-white items-center flex justify-center rounded-xl">
+            
+        </div>
+
+    </div>
+
+{/if}
+
+
+
+<!-- <style>
+
+</style> -->
 
 
 <!-- <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>  -->
