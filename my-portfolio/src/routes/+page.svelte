f06c4d331fa7a60035e9e6e59b7134fc67b3b805
Simone Chrastek <simonechrastek@Simones-MacBook-Air.local>
2026-02-22 12:18:34 -0500

    initial svelte project setup

---
+++ my-portfolio/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<div class = "container">
+    <h1>simone chrastek</h1>
+    <h2>has invited you to chat!</h2>
+</div>
+
+<style>
+</style>
+
+
+<!-- <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>  -->
